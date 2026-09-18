31bfd63e8b9959fc85f194460e4546e26159b470
dimasryabov22-gif <dimasryabov22@gmail.com>
2026-09-18 18:17:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("Привет! 👋 Бот работает!")
+TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):

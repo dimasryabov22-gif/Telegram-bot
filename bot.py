d2a06aef96b2def1443d37946c9702b096bf43bc
dimasryabov22-gif <dimasryabov22@gmail.com>
2026-09-18 17:56:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,23 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Привет! Я бот для проверки собственного IP.\n"
-        "Telegram не передаёт мне твой IP-адрес напрямую."
+        "Привет! 👋 Бот работает."
     )
+
 
 def main():
     app = Application.builder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+
+    print("Бот запущен...")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
